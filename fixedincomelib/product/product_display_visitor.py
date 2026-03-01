e66012214e75295f6091713e751e95e22ad55bb1
Chua Yu Xuan Jordan <jcyx1999@gmail.com>
2026-03-01 17:54:07 -0500

    Done

--- fixedincomelib/product/product_display_visitor.py
+++ fixedincomelib/product/product_display_visitor.py
@@ -26,10 +26,61 @@ class ProductDisplayVisitor(ProductVisitor):
     def display(self) -> pd.DataFrame:
         return pd.DataFrame(self.nvps_, columns=["Name", "Value"])
 
+    # We introduce the @visit.register syntax since visit is a dynamic method and therefore we require 
+    # this syntax so our class object knows which method to use based on the product type 
     # TODO: ProductBulletCashflow
+    @visit.register
+    def _(self, product: "ProductBulletCashflow"): 
+        self.nvps_.append(["Product Type", product.product_type])
+        self.nvps_.append(["Notional", product.notional])
+        self.nvps_.append(["Currency", product.currency.value_str])
+        self.nvps_.append(["Long or Short", product.long_or_short.to_string().upper()])
+        self.nvps_.append(["Termination Date", product.termination_date.ISO()])
+        self.nvps_.append(["Payment Date", product.payment_date.ISO()])
 
     # TODO: ProductFixedAccrued
+    @visit.register
+    def _(self, product: "ProductFixedAccrued"): 
+        self.nvps_.append(["Product Type", product.product_type])
+        self.nvps_.append(["Notional", product.notional])
+        self.nvps_.append(["Currency", product.currency.value_str])
+        self.nvps_.append(["Long or Short", product.long_or_short.to_string().upper()])
+        self.nvps_.append(["Effective Date", product.effective_date.ISO()])
+        self.nvps_.append(["Termination Date", product.termination_date.ISO()])
+        self.nvps_.append(["Accrual Basis", product.accrual_basis.value_str])
+        self.nvps_.append(["Accrued", product.accrued])
 
     # TODO: ProductOvernightIndexCashflow
+    @visit.register
+    def _(self, product: "ProductOvernightIndexCashflow"): 
+        self.nvps_.append(["Product Type", product.product_type])
+        self.nvps_.append(["Notional", product.notional])
+        self.nvps_.append(["Currency", product.currency.value_str])
+        self.nvps_.append(["Long or Short", product.long_or_short.to_string().upper()])
+        self.nvps_.append(["Effective Date", product.effective_date.ISO()])
+        self.nvps_.append(["Termination Date", product.termination_date.ISO()])
+        self.nvps_.append(["Payment Date", product.payment_date.ISO()])
+        self.nvps_.append(["Overnight Index", product.on_index.name()])
+        self.nvps_.append(["Compounding Method", product.compounding_method.to_string().upper()])
+        self.nvps_.append(["Spread", product.spread])
 
     # TODO: ProductRFRSwap
+    @visit.register
+    def _(self, product: "ProductRFRSwap"): 
+        self.nvps_.append(["Product Type", product.product_type])
+        self.nvps_.append(["Notional", product.notional])
+        self.nvps_.append(["Currency", product.currency.value_str])
+        self.nvps_.append(["Long or Short", product.long_or_short.to_string().upper()])
+        self.nvps_.append(["Effective Date", product.effective_date.ISO()])
+        self.nvps_.append(["Termination Date", product.termination_date.ISO()])
+        self.nvps_.append(["Payment Offset", product.pay_offset.__str__()])
+        self.nvps_.append(["Overnight Index", product.on_index.name()])
+        self.nvps_.append(["Compounding Method", product.compounding_method.to_string().upper()])
+        self.nvps_.append(["Spread", product.spread])
+        self.nvps_.append(["Fixed Rate", product.fixed_rate])
+        self.nvps_.append(["Pay or Receive", product.pay_or_rec.to_string().upper()])
+        self.nvps_.append(["Accrual Basis", product.accrual_basis.value_str])
+        self.nvps_.append(["Fixed Leg Accrual Period", product.accrual_period.__str__()])
+        self.nvps_.append(["Floating Leg Accrual Period", product.floating_leg_accrual_period.__str__()])
+        self.nvps_.append(['Business Day Convention', product.pay_business_day_convention.value_str])
+        self.nvps_.append(['Holiday Convention', product.pay_holiday_convention.value_str])
